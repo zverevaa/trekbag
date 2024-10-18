1f19c97560286f7ebf419bba0b163b57022c4575
Kira Zvereva <angryvex@gmail.com>
2024-10-18 22:28:36 +0300

    Fix typo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer>
-            <span>&copy; 2024. Copyright by Aisu</span>
+            <span>&copy; 2024. Copyright</span>
             <span>
                 Version <b>1.5</b>
             </span>
